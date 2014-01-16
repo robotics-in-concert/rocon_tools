8bb735dec1368e016730fd938b225cd9d8cdca72
Daniel Stonier <d.stonier@gmail.com>
2014-01-17 08:35:18 +0900

    trivial comment updates.

--- rocon_console/setup.py
+++ rocon_console/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['rocon_console'],
     package_dir={'': 'src'},
-#    scripts=['scripts/concert_roles_and_apps',
+#    scripts=['scripts/rocon_interactions',
 #             ],
 )
 

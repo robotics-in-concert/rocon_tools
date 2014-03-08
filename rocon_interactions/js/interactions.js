7d77cfe3f7f4da735f2495f7b55873088a55cd6f
Daniel Stonier <d.stonier@gmail.com>
2014-03-08 22:30:32 +0900

    interactions javascript and listener html.

--- rocon_interactions/js/interactions.js
+++ rocon_interactions/js/interactions.js
@@ -7,40 +7,26 @@ function queryFieldByName(name) {
     return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-function queryInteractionParameters() {
-    // TODO : remove rosbridge_address adn rosbrdige_port variables
-    // from here as they are typically not web app relevant
-    param_string = queryFieldByName('params')
-    // could use some error checking to make sure it doesn't return null here.
-    return JSON.parse(param_string)
-}
-
-function queryInteractionsRosbridgeURI() {
-    // TODO : remove rosbridge_address adn rosbrdige_port variables
-    // from here as they are typically not web app relevant
-	param_string = queryFieldByName('params')
-	// could use some error checking to make sure it doesn't return null here.
-	params = JSON.parse(param_string)
-	// check if rosbridge_address and rosbridge_port are not present and
-	// substitute with localhost:9090 if so.
-	return 'ws://' + params['rosbridge_address'] + ':' + params['rosbridge_port'] 
-}
-
-function queryInteractionRemaps() {
-	// This goes quite a bit further than directly returning the remaps json string
-	// as an object. It takes th interactions specification and parses the list
-	// converting it into a dictionary with keys as the remap froms and values
-	// as the remap tos'. This is useful for working with, wherase the former
-	// is useful in terms of ros api.
-
-	remaps_string = queryFieldByName('remaps')
-	// could use some error checking to make sure it doesn't return null here.
-	o = JSON.parse(remaps_string)
-	// convert this from the list of 'remap_from': 'xyz', 'remap_to': 'zyx' dictionaries to
-	// a single dictionary with remap_from keys and remap_to pairs.
-	remaps = {}
-	for (var i = 0; i < o.length; i++) {
-        remaps[o[i]['remap_from']] = o[i]['remap_to']
+// Singleton object
+var rocon_interactions = new function() {
+    decoded_interactions_query_string = queryFieldByName('interaction_data')
+    interaction_data = JSON.parse(decoded_interactions_query_string)
+    this.display_name = interaction_data['display_name']
+    this.rosbridge_uri = 'ws://'
+    if ('rosbridge_address' in interaction_data['parameters']) {
+        this.rosbridge_uri += interaction_data['parameters']['rosbridge_address']
+        //delete interaction_data['parameters']['rosbridge_address']
+    } else {
+        this.rosbridge_uri += 'localhost'
+    } 
+    if ('rosbridge_port' in interaction_data['parameters']) {
+        this.rosbridge_uri += ':' + interaction_data['parameters']['rosbridge_port']
+        //delete interaction_data['parameters']['rosbridge_port']
+    } else {
+        this.rosbridge_uri += ':9090' 
     }
-    return remaps
+    this.parameters = interaction_data['parameters'] 
+    this.remappings = interaction_data['remappings']
 }
+
+

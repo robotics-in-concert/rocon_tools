f0c97c3e04e8f637f68a781c9fb1fb823830ac28
Daniel Stonier <d.stonier@gmail.com>
2014-03-18 00:11:15 +0900

    pep8

--- rocon_python_utils/src/rocon_python_utils/ros/__init__.py
+++ rocon_python_utils/src/rocon_python_utils/ros/__init__.py
@@ -8,5 +8,5 @@
 
 from .catkin import package_index_from_package_path
 from .rosdistro import get_rosdistro
-from .resources import find_resource, find_resource_from_string, resource_index_from_package_exports 
+from .resources import find_resource, find_resource_from_string, resource_index_from_package_exports
 from .icons import *

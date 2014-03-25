b3e008af0bfc4465606239871a2f204f57484ab1
Daniel Stonier <d.stonier@gmail.com>
2014-03-25 12:16:09 +0900

    install rule fixes for subpackages.

--- rocon_python_utils/setup.py
+++ rocon_python_utils/setup.py
@@ -4,10 +4,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['rocon_python_utils'],
+    packages=['rocon_python_utils', 'rocon_python_utils.network', 'rocon_python_utils.ros', 'rocon_python_utils.system'],
     package_dir={'': 'src'},
-#    scripts=['scripts/rocon_interactions',
-#             ],
 )
 
 setup(**d)

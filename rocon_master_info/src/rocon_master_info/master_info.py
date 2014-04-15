007e8c7ccb8fdae430a4a1bde0e0e2937786bab3
Daniel Stonier <d.stonier@gmail.com>
2014-04-15 17:59:38 +0900

    move master info code to a common spot

--- rocon_master_info/src/rocon_master_info/master_info.py
+++ rocon_master_info/src/rocon_master_info/master_info.py
@@ -11,7 +11,7 @@ import sys
 import rospy
 import rocon_python_comms
 import rocon_python_utils
-import rocon_console.console as console
+import rocon_console.console as rocon_console
 import rocon_std_msgs.msg as rocon_std_msgs
 
 ##############################################################################
@@ -55,78 +55,40 @@ def get_master_info(timeout=1.0):
         master_info = result  # rocon_std_msgs.MasterInfo
     return master_info
 
-##############################################################################
-# Qt Classes
-##############################################################################
-
-qt_available = True
-
-try:
-    import signal
-    from python_qt_binding.QtGui import QWidget, QPixmap, QLabel, QIcon, QCursor, QApplication
-    from python_qt_binding.QtCore import Qt
-
-    class Window(QWidget):
-
-        def __init__(self, title, name, description, version, icon):
-            super(Window, self).__init__()
-            self._title = title
-            self._description = description
-            self._icon = icon
-            self._name = name
-            self._version = version
-            self.initUI()
-
-        def initUI(self):
-            pixmap = QPixmap(self._icon)
-            self.pic = QLabel(self)
-            self.pic.setPixmap(pixmap)
-            self.pic.resize(pixmap.width(), pixmap.height())
-            #self.pic.setGeometry(10, 10, pixmap.width(), pixmap.height())
-
-            self.text = QLabel(self)
-            self.text.resize(200, pixmap.height())
-            self.text.move(pixmap.width() + 10, 0)
-            self.text.setText("<b>Name:</b> %s<br/><b>Rocon Version:</b> %s<br/><b>Description:</b> %s" % (self._name, self._version, self._description))
-            self.text.setWordWrap(True)
-            self.text.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
-
-            self.setWindowTitle(self._title)
-            self.setWindowIcon(QIcon(self._icon))
-            self.putUnderMouse()
-
-        def putUnderMouse(self):
-            mouse = QCursor.pos()
-            self.move(mouse.x() - (210 + self.pic.geometry().width()) / 2,
-                        mouse.y() - (self.pic.geometry().height()) / 2)
-
-except ImportError:
-    qt_available = False
-
 
 ##############################################################################
 # Main
 ##############################################################################
 
-def main(node_name='master_info', title='Master Information'):
+def console_only_main(node_name='master_info', title='Master Information'):
     '''
       Establishes a connection and prints master information to the console.
     '''
 
     rospy.init_node(node_name)
     master_info = get_master_info()
+#     display_available = True if 'DISPLAY' in os.environ.keys() else False
+
+    rocon_console.pretty_println(title, rocon_console.bold)
+    print(rocon_console.cyan + "  Name       : " + rocon_console.yellow + master_info.name + rocon_console.reset)
+    print(rocon_console.cyan + "  Description: " + rocon_console.yellow + master_info.description + rocon_console.reset)
+    print(rocon_console.cyan + "  Icon       : " + rocon_console.yellow + master_info.icon.resource_name + rocon_console.reset)
+    print(rocon_console.cyan + "  Version    : " + rocon_console.yellow + master_info.version + rocon_console.reset)
+
+
+def main(node_name='master_info', title='Master Information', console=True):
     display_available = True if 'DISPLAY' in os.environ.keys() else False
+    try:
+        import rocon_qt_master_info
+        from rqt_gui.main import Main
+        qt_available = True
+    except ImportError:
+        qt_available = False
+        if display_available and not console:
+            print(rocon_console.red + "WARNING: rqt plugin not found, console output only (hint: install rocon_qt_master_info)." + rocon_console.reset)
 
-    console.pretty_println(title, console.bold)
-    print(console.cyan + "  Name       : " + console.yellow + master_info.name + console.reset)
-    print(console.cyan + "  Description: " + console.yellow + master_info.description + console.reset)
-    print(console.cyan + "  Icon       : " + console.yellow + master_info.icon.resource_name + console.reset)
-    print(console.cyan + "  Version    : " + console.yellow + master_info.version + console.reset)
-
-    if qt_available and display_available:
-        icon = rocon_python_utils.ros.find_resource_from_string(master_info.icon.resource_name)
-        signal.signal(signal.SIGINT, signal.SIG_DFL)  # make sure this comes after the rospy call, otherwise it will handle signals.
-        app = QApplication(sys.argv)
-        window = Window(title, master_info.name, master_info.description, master_info.version, icon)
-        window.show()
-        sys.exit(app.exec_())
+    if console or not display_available or not qt_available:
+        console_only_main(node_name, title)
+    else:
+        main = Main()
+        sys.exit(main.main(argv=sys.argv, standalone='rocon_qt_master_info'))

3938199ac1b3fc772b416c9dcf61626c7eef861e
Daniel Stonier <d.stonier@gmail.com>
2014-05-04 22:56:58 +0900

    rocon_master_info sphinxed.

--- rocon_master_info/src/rocon_master_info/__init__.py
+++ rocon_master_info/src/rocon_master_info/__init__.py
@@ -3,6 +3,18 @@
 #   https://raw.github.com/robotics-in-concert/rocon_tools/license/LICENSE
 #
 ##############################################################################
+# Documentation
+##############################################################################
+
+"""
+This is the top-level namespace of the rocon_master_info_ ROS
+package. It provides a ros node for advertising basic information about
+a ros master.
+
+.. _rocon_master_info: http://wiki.ros.org/rocon_master_info
+
+"""
+##############################################################################
 # Imports
 ##############################################################################
 

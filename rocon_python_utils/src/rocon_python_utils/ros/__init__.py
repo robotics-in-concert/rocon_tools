5857d9757d9f8ea45634195c133341b77ee66b18
dwlee <dwlee@yujinrobot.com>
2015-01-20 15:20:46 +0900

    create new tools about handing name string and path

--- rocon_python_utils/src/rocon_python_utils/ros/__init__.py
+++ rocon_python_utils/src/rocon_python_utils/ros/__init__.py
@@ -10,3 +10,5 @@ from .catkin import package_index_from_package_path
 from .rosdistro import get_rosdistro
 from .resources import find_resource, find_resource_from_string, resource_index_from_package_exports
 from .icons import *
+from .names import get_ros_friendly_name, check_extension_name
+from .paths import get_rocon_home

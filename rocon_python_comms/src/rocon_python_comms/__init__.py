8175f4bdfc3e5c92b8cc883dedc087dd83d2dc56
Jihoon Lee <jihoonlee.in@gmail.com>
2015-03-02 18:47:27 +0900

    rename is_valid_service

--- rocon_python_comms/src/rocon_python_comms/__init__.py
+++ rocon_python_comms/src/rocon_python_comms/__init__.py
@@ -22,7 +22,7 @@ from .exceptions import *
 from .nodes import find_node
 from .service_pair_client import ServicePairClient
 from .service_pair_server import ServicePairServer
-from .services import find_service, is_valid_service
+from .services import find_service, service_is_available
 from .subscriber_proxy import SubscriberProxy
 from .topics import find_topic
 from .wall_rate import WallRate

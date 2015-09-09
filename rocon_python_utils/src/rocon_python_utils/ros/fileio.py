9210c27f5a811c7468003cb72a373c55bb6b63d9
Daniel Stonier <d.stonier@gmail.com>
2015-09-10 00:06:10 +0900

    [rocon_python_utils] pep8.

--- rocon_python_utils/src/rocon_python_utils/ros/fileio.py
+++ rocon_python_utils/src/rocon_python_utils/ros/fileio.py
@@ -31,9 +31,10 @@ import rospkg
 # Paths
 ##############################################################################
 
+
 def get_rocon_home():
     '''
-      Retrieve the location of the rocon home directory for using rocon compoenets. 
+      Retrieve the location of the rocon home directory for using rocon components.
       If it is not existed, create new directory and return this path
 
       @return the service manager home directory (path object).
@@ -44,18 +45,18 @@ def get_rocon_home():
         os.makedirs(rocon_home)
     return os.path.join(rospkg.get_ros_home(), 'rocon')
 
-def is_validation_file(file_path):
-  '''
-  Investigate whether file is validated or not as checking file existed in path and file size
 
-  :param file_path: full file path to check
-  :type file_path: string
-  
-  :return: validation of file
-  :rtype: bool
-  '''
-  is_validated = True
-  if not os.path.isfile(file_path) or os.stat(file_path).st_size <= 0:
-      is_validated = False
-  
-  return is_validated
+def is_validation_file(file_path):
+    '''
+    Investigate whether file is validated or not as checking file existed in path and file size
+
+    :param file_path: full file path to check
+    :type file_path: string
+
+    :return: validation of file
+    :rtype: bool
+    '''
+    is_validated = True
+    if not os.path.isfile(file_path) or os.stat(file_path).st_size <= 0:
+        is_validated = False
+    return is_validated

f26152beb64512b41ec5052935fd712f9a6382ce
Daniel Stonier <d.stonier@gmail.com>
2016-01-11 00:48:40 +0900

    [rocon_python_comms] bugfix data updates in subscriber proxy

--- rocon_python_comms/tests/rostests/subscriber_proxy/latched_talker.py
+++ rocon_python_comms/tests/rostests/subscriber_proxy/latched_talker.py
@@ -4,7 +4,7 @@ import rospy
 from std_msgs.msg import String
 
 def talker():
-    pub = rospy.Publisher('chatter', String, latch=True)
+    pub = rospy.Publisher('chatter', String, latch=True, queue_size=5)
     rospy.init_node('talker')
     while not rospy.is_shutdown():
         str = "dude"
